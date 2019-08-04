0456c7c9f51b11c63be03085a53752ab0deccef5
Erislândio Soares <erislandio.soares@acct.global>
2019-08-04 16:33:18 -0300

    add movinment nave and object sprites

--- js/script.js
+++ js/script.js
@@ -5,14 +5,16 @@
     var sprites = [];
     var assetsToLoad = [];
 
-    // ! imagem
+    var background = new Sprite(0, 56, 400, 500, 0, 0);
+    var nave = new Sprite(0, 0, 30, 50, 185, 450);
+
+    sprites.push(background, nave);
 
     var img = new Image();
     img.addEventListener("load", loadHandler, false);
     img.src = "./assets/img.png";
     assetsToLoad.push(img);
 
-    // ! contador de recursos
     let loadedAssets = 0;
 
     const left = 37,
@@ -82,9 +84,44 @@
         render();
     }
 
-    function update() {}
+    function update() {
+        if (mvLeft && !mvRight) {
+            nave.vx = -5;
+        }
 
-    function render() {}
+        if (mvRight && !mvLeft) {
+            nave.vx = 5;
+        }
+
+        if (!mvLeft && !mvRight) {
+            nave.vx = 0;
+        }
+
+        nave.x = Math.max(
+            0,
+            Math.min(cnv.width - nave.width, nave.x + nave.vx)
+        );
+    }
+
+    function render() {
+        ctx.clearRect(0, 0, cnv.width, cnv.height);
+        if (sprites.length != 0) {
+            for (let i = 0; i < sprites.length; i++) {
+                const spr = sprites[i];
+                ctx.drawImage(
+                    img,
+                    spr.sourceX,
+                    spr.sourceY,
+                    spr.width,
+                    spr.height,
+                    Math.floor(spr.x),
+                    Math.floor(spr.y),
+                    spr.width,
+                    spr.height
+                );
+            }
+        }
+    }
 
     function loadHandler() {
         loadedAssets++;
