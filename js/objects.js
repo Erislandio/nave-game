48aadc44ee7813cafd1b1fa8004bf9faf3eadd6d
Erislândio Soares <erislandio.soares@acct.global>
2019-08-04 16:02:12 -0300

    add funtions

--- js/objects.js
+++ js/objects.js
@@ -1,39 +1,78 @@
 (function() {
-    // * canvas
-    var cnv = document.querySelector("canvas");
-    // * contexto de renderização 2d
-    var ctx = cnv.getContext("2d");
+    const cnv = document.querySelector("canvas");
+    const ctx = cnv.getContext("2d");
 
-    // * recursos do game
-
-    // entradas
-    var left = 37,
+    const left = 37,
         right = 39,
         enter = 13,
         space = 32;
 
-    // direções
     var mvLeft = (mvRight = false);
-
-    // estados do jogo
 
     var loading = 0,
         playing = 1,
         paused = 2,
         over = 3;
-
     var gameState = loading;
-
-    // listeners
 
     window.addEventListener(
         "keydown",
-        function(e) {
+        e => {
             var key = e.keyCode;
-            this.alert(key);
+            switch (key) {
+                case left:
+                    mvLeft = true;
+                    break;
+                case right:
+                    mvRight = true;
+                    break;
+            }
+
+            console.log(key);
         },
         false
     );
 
-    // ? funções
+    window.addEventListener("keyup", e => {
+        var key = e.keyCode;
+        switch (key) {
+            case left:
+                mvLeft = false;
+                break;
+            case right:
+                mvRight = false;
+                break;
+            case enter:
+                if (gameState !== playing) {
+                    gameState = playing;
+                } else {
+                    gameState = paused;
+                }
+        }
+
+        console.log(key);
+    });
+
+    function loop() {
+        requestAnimationFrame(loop, cnv);
+
+        switch (gameState) {
+            case loading:
+                console.log("LOADING...");
+                break;
+            case playing:
+                update();
+                break;
+        }
+
+        render();
+    }
+
+    function update() {}
+
+    function render() {
+        alert("rende");
+    }
+
+    loop();
 })();
